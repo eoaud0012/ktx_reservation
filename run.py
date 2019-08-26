93dc36ef1b3921c2eb9deb9c327aed314258ed3f
Park youngjin <dudwls251@naver.com>
2019-08-26 11:25:53 +0900

    Add root exception

--- run.py
+++ run.py
@@ -1,17 +1,22 @@
 import time
+import traceback
 from datetime import datetime
 
 from src.request_to_korail import login, train_search, is_login, after_reserve, driver
+from src.send_slacker import send_msg
 
 cnt = 0
-login()
-train_search()
-while not train_search(True):
-    cnt += 1
-    print("[%s]%d 번째 시도중" % (datetime.now(), cnt))
-    # time.sleep(1)
-    is_login()
+try:
+    login()
+    train_search()
+    while not train_search(True):
+        cnt += 1
+        print("[%s]%d 번째 시도중" % (datetime.now(), cnt))
+        # time.sleep(1)
+        is_login()
 
-after_reserve()
-
-driver.quit()
+    after_reserve()
+except:
+    send_msg("꺼짐 \n %s" % traceback.print_exc())
+finally:
+    driver.quit()
